c83072d61358818b598291c38e845c892aed349c
Hathaway-Shan <13614545+Hathaway-Shan@users.noreply.github.com>
2022-06-30 12:38:11 -0700

    Revert "Dev"

--- app.js
+++ app.js
@@ -1,36 +1,19 @@
 // import services and utilities
-import state from './state.js';
-import { getPosts } from './services/Services.js';
-import createPostDisplay from './components/PostDisplay.js';
-
 
 // import component creators
 
 // declare state variables
 
-
 // write handler functions
-
-//displays post strings from database
-async function handlePageLoad() {
-    state.bulletin = await getPosts();
-    display();
-}
 
 // Create each component: 
 // - pass in the root element via querySelector
-const PostDisplay = createPostDisplay(document.querySelector('#bulletin-board'));
-
-
-
 // - pass any needed handler functions as properties of an actions object 
 
 // Roll-up display function that renders (calls with state) each component
 function display() {
     // Call each component passing in props that are the pieces of state this component needs
-    PostDisplay({ bulletin: state.bulletin });
-
 }
 
 // Call display on page load
-handlePageLoad();
+display();
--- utils.js
+++
@@ -1,8 +0,0 @@
-import { getUser } from './services/Services.js';
-
-export async function protectUser() {
-    const user = await getUser;
-    if (!user) {
-        location.replace('../members/' + window.location.search);
-    }
-}
